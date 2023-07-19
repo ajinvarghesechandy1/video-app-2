325d4efe9661e32cf60f8431b9e476571647a54f
ajinvarghesechandy <chandyajin@gmail.com>
2023-07-19 19:47:43 +0530

    firebase setup completed

--- src/lib/firbase.ts
+++ src/lib/firbase.ts
@@ -1,1 +1,16 @@
-import { FirebaseApp, auth } from 'firebase';
+import {initializeApp  } from 'firebase/app';
+import {signInWithEmailAndPassword , onAuthStateChanged} from 'firebase/auth'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhcj0QDPbem1XpohFoeJOUaR_zsC_nykg",
+    authDomain: "fir-6b335.firebaseapp.com",
+    databaseURL: "https://fir-6b335-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fir-6b335",
+    storageBucket: "fir-6b335.appspot.com",
+    messagingSenderId: "445228741280",
+    appId: "1:445228741280:web:00b6c15d29b9bcb556cfc5",
+    measurementId: "G-JVM3R66GG3"
+  };
+const app = initializeApp(firebaseConfig)
+
+export {signInWithEmailAndPassword , onAuthStateChanged}
